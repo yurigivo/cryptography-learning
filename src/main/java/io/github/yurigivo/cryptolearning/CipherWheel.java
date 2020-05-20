401a2e4cc258d2a7b1afa96a76f579a582c32e7e
Iurii Volobuev <iurii.v.dev@gmail.com>
2020-05-21 02:54:28 +0300

    completed exercise 1.1

--- src/main/java/io/github/yurigivo/cryptolearning/CipherWheel.java
+++ src/main/java/io/github/yurigivo/cryptolearning/CipherWheel.java
@@ -1,6 +1,8 @@
 package io.github.yurigivo.cryptolearning;
 
 public class CipherWheel {
+    private static final char START_LETTER = 'a';
+    public static final int ALPHABET_LENGTH = 26;
     private final int rotation;
 
     public CipherWheel(int rotation) {
@@ -10,13 +12,13 @@ public class CipherWheel {
     public String encrypt(String plaintext) {
         StringBuilder result = new StringBuilder(plaintext.length());
         for (char next : plaintext.toCharArray())
-            result.append((char) (next + rotation));
+            result.append((char) ((next - START_LETTER + rotation) % ALPHABET_LENGTH + START_LETTER));
         return result.toString();
     }
     public String decrypt(String ciphertext) {
         StringBuilder result = new StringBuilder(ciphertext.length());
         for (char next : ciphertext.toCharArray())
-            result.append((char) (next - rotation));
+            result.append((char) ((next - START_LETTER - rotation + ALPHABET_LENGTH) % ALPHABET_LENGTH + START_LETTER));
         return result.toString();
     }
 }
--- src/test/java/io/github/yurigivo/cryptolearning/CipherWheelTest.java
+++ src/test/java/io/github/yurigivo/cryptolearning/CipherWheelTest.java
@@ -2,12 +2,47 @@ package io.github.yurigivo.cryptolearning;
 
 import org.junit.Test;
 
-
+import static io.github.yurigivo.cryptolearning.CipherWheel.ALPHABET_LENGTH;
 import static org.junit.Assert.assertEquals;
+
 public class CipherWheelTest {
     @Test public void encryptsAndDecryptsMessage_whenShiftingInsideAlphabet() {
+        CipherWheel cipherWheel = new CipherWheel(2);
+        assertEquals("cde", cipherWheel.encrypt("abc"));
+        assertEquals("abc", cipherWheel.decrypt("cde"));
+    }
+    @Test public void lettersStayTheSame_whenNilRotation() {
+        CipherWheel cipherWheel = new CipherWheel(0);
+        assertEquals("abc", cipherWheel.encrypt("abc"));
+        assertEquals("abc", cipherWheel.decrypt("abc"));
+    }
+    @Test public void goesAround_whenReachingEndOfAlphabet() {
         CipherWheel cipherWheel = new CipherWheel(1);
-        assertEquals("bcd", cipherWheel.encrypt("abc"));
-        assertEquals("abc", cipherWheel.decrypt("bcd"));
+        assertEquals("a", cipherWheel.encrypt("z"));
+        assertEquals("z", cipherWheel.decrypt("a"));
+    }
+
+    /** Ex. 1.1 (a) */
+    @Test public void encryptsWithRotation11() {
+        String plaintext = "A page of history is worth a volume of logic.";
+        String ciphertext = new CipherWheel(11).encrypt(plaintext);
+        System.out.println(ciphertext.toUpperCase());
+    }
+    /** Ex. 1.1 (b) */
+    @Test public void decryptsWithRotation7() {
+        String plaintext = "AOLYLHYLUVZLJYLAZILAALYAOHUAOLZLJYLAZAOHALCLYFIVKFNBLZZLZ";
+        String ciphertext = new CipherWheel(7).decrypt(plaintext.toLowerCase());
+        System.out.println(ciphertext);
+    }
+    /** Ex. 1.1 (c) */
+    @Test public void decryptsWithRotation_changingByOneForEachLetter() {
+        String ciphertext = "XJHRFTNZHMZGAHIUETXZJNBWNUTRHEPOMDNBJMAUGORFAOIZOCC";
+        StringBuilder plaintext = new StringBuilder();
+        char[] letters = ciphertext.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            String letter = String.valueOf(letters[i]).toLowerCase();
+            plaintext.append(new CipherWheel((i + 1) % ALPHABET_LENGTH).decrypt(letter));
+        }
+        System.out.println(plaintext);
     }
 }
